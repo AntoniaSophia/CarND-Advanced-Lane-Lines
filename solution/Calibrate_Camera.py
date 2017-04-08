a4329100ad9c4f2cece091786dbffcf21fde4f60
Antonia Reiter <antonia.reiter@freenet.de>
2017-04-08 09:51:51 +0200

    First pipeline with warp

--- solution/Calibrate_Camera.py
+++ solution/Calibrate_Camera.py
@@ -2,11 +2,27 @@ import numpy as np
 import cv2
 import glob
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+from matplotlib.patches import Polygon
 import pickle
 
+
+# Event listener, if button of mouse is clicked
+def onclick(event):
+    global ix, iy
+    ix, iy = event.xdata, event.ydata
+    #print the x,y coordinates of mouse
+    print(ix, iy)
+    return
+
+
+
+nx = 9 # the number of inside corners in x
+ny = 6 # the number of inside corners in y
+
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((6*9,3), np.float32)
-objp[:,:2] = np.mgrid[0:9, 0:6].T.reshape(-1,2)
+objp = np.zeros((ny*nx,3), np.float32)
+objp[:,:2] = np.mgrid[0:nx, 0:ny].T.reshape(-1,2)
 
 
 
@@ -23,7 +39,7 @@ for idx, fname in enumerate(images):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     # Find the chessboard corners
-    ret, corners = cv2.findChessboardCorners(gray, (9,6), None)
+    ret, corners = cv2.findChessboardCorners(gray, (nx,ny), None)
 
     # If found, add object points, image points
     if ret == True:
@@ -31,38 +47,114 @@ for idx, fname in enumerate(images):
         imgpoints.append(corners)
         #print(corners)
         # Draw and display the corners
-        cv2.drawChessboardCorners(img, (9,6), corners, ret)
+        #cv2.drawChessboardCorners(img, (9,6), corners, ret)
         #write_name = 'corners_found'+str(idx)+'.jpg'
         #cv2.imwrite(write_name, img)
-        cv2.imshow('img', img)
-        cv2.waitKey(5000)
+        #cv2.imshow('img', img)
+        #cv2.waitKey(5000)
+
+
+
+def calculateWarp(img, mtx, dist):
+    # Use the OpenCV undistort() function to remove distortion
+    undist = cv2.undistort(img, mtx, dist, None, mtx)
+    # Convert undistorted image to grayscale
+    gray = cv2.cvtColor(undist, cv2.COLOR_BGR2GRAY)
+    # Search for corners in the grayscaled image
+
+    # define the 4 coordinates (right top, right bottom, left bottom, left top) in the source image
+    src = np.float32(
+        [[703 , 460],
+         [1104 , 718],
+         [207 , 718],
+         [578 , 460]])
+
+
+
+
+
+    # define the 4 desired coordinates (right top, right bottom, left bottom, left top) in the source image
+    dst = np.float32(
+        [[900,0],
+         [900,718],
+         [300,718],
+         [300,0]])
+
+    # Compute the perspective transform, M, given source and destination points
+    M = cv2.getPerspectiveTransform(src, dst)
+
+    # Compute the inverse perspective transform
+    Minv = cv2.getPerspectiveTransform(dst, src)
+    
+    # Warp an image using the perspective transform, M
+    warped = cv2.warpPerspective(img, M, img_size, flags=cv2.INTER_LINEAR)
+    # Return the resulting image and matrix
+    return warped, M
 
 
 #cv2.destroyAllWindows()
+calcDistortion = True
+calcWarp = True
 
 # Test undistortion on an image
-#img = cv2.imread('calibration_wide/test_image.jpg')
-img = cv2.imread('../camera_cal/calibration2.jpg')
+if calcDistortion == True:
+    #img = cv2.imread('calibration_wide/test_image.jpg')
+    img = cv2.imread('../camera_cal/calibration13.jpg')
 
-img_size = (img.shape[1], img.shape[0])
+    img_size = (img.shape[1], img.shape[0])
 
-# Do camera calibration given object points and image points
-ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, img_size,None,None)
+    # Do camera calibration given object points and image points
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, img_size,None,None)
 
 
-dst = cv2.undistort(img, mtx, dist, None, mtx)
-cv2.imwrite('../camera_cal/test_undistored_calibration3.jpg',dst)
+    #dst = cv2.cvtColor(dst, cv2.COLOR_BGR2RGB)
+    dst = cv2.undistort(img, mtx, dist, None, mtx)
+    #cv2.imwrite('../camera_cal/test_undistored_calibration13.jpg',dst)
+
+
+
+if calcWarp == True:
+    img = cv2.imread('../test_images/straight_lines1.jpg')
+    img_dist = cv2.undistort(img, mtx, dist, None, mtx)
+    fig = plt.figure(1)
+    #To print out position of Mouse if left button clicked
+    cid = fig.canvas.mpl_connect('button_press_event', onclick)
+    plt.imshow(img_dist)
+    plt.show()    
+    unwarped_image, perspective_M = calculateWarp(img,mtx, dist)
+
+
+
+
+
 
 # Save the camera calibration result for later use (we won't worry about rvecs / tvecs)
-dist_pickle = {}
-dist_pickle["mtx"] = mtx
-dist_pickle["dist"] = dist
-pickle.dump( dist_pickle, open( "../camera_cal/camera_calibration_pickle.p", "wb" ) )
-#dst = cv2.cvtColor(dst, cv2.COLOR_BGR2RGB)
 # Visualize undistortion
-f, (ax1, ax2) = plt.subplots(1, 2, figsize=(20,10))
-ax1.imshow(img)
-ax1.set_title('Original Image', fontsize=30)
-ax2.imshow(dst)
-ax2.set_title('Undistorted Image', fontsize=30)
-plt.show()
+if  calcDistortion == True and calcWarp == True:
+    img = cv2.imread('../test_images/straight_lines1.jpg')
+    dst = cv2.undistort(img, mtx, dist, None, mtx)
+    unwarped_image, perspective_M = calculateWarp(img, mtx, dist)
+
+    polygon = Polygon([[703 , 460], [1104 , 718], [203 , 718], [578 , 460]], closed=True, fill=True, linewidth=2,edgecolor='r',facecolor='none')
+    rectangle = patches.Rectangle((300,2),600,718,linewidth=2,edgecolor='r',facecolor='none')
+
+
+    f, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(20,10))
+    ax1.imshow(img)
+    ax1.set_title('Original Image', fontsize=30)
+    ax2.imshow(dst)
+    ax2.add_patch(polygon)
+    ax2.set_title('Undistorted Image', fontsize=30)
+    ax3.imshow(unwarped_image)
+    ax3.add_patch(rectangle)
+    ax3.set_title('Unwarped Image', fontsize=30)
+    plt.show()
+
+    dist_pickle = {}
+    dist_pickle["mtx"] = mtx
+    dist_pickle["dist"] = dist
+    dist_pickle["rvecs"] = rvecs
+    dist_pickle["tvecs"] = tvecs
+    dist_pickle["M"] = perspective_M
+
+    pickle.dump( dist_pickle, open( "../camera_cal/camera_calibration_pickle.p", "wb" ) )
--- solution/Pipeline.py
+++ solution/Pipeline.py
@@ -4,23 +4,60 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
+
+def warp(img,warp_Matrix):
+	img_size = (img.shape[1], img.shape[0])
+	warped = cv2.warpPerspective(img, warp_Matrix, img_size,flags=cv2.INTER_LINEAR)
+	return warped
+
+
+def undistort(img):
+	return cv2.undistort(img, mtx, dist, None, mtx)
+
+def colorGradient(img, s_thresh=(180, 255), sx_thresh=(30, 120)):
+    img = np.copy(img)
+    # Convert to HSV color space and separate the V channel
+    hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HLS).astype(np.float)
+    l_channel = hsv[:,:,1]
+    s_channel = hsv[:,:,2]
+    # Sobel x
+    sobelx = cv2.Sobel(l_channel, cv2.CV_64F, 1, 0) # Take the derivative in x
+    abs_sobelx = np.absolute(sobelx) # Absolute x derivative to accentuate lines away from horizontal
+    scaled_sobel = np.uint8(255*abs_sobelx/np.max(abs_sobelx))
+    
+    # Threshold x gradient
+    sxbinary = np.zeros_like(scaled_sobel)
+    sxbinary[(scaled_sobel >= sx_thresh[0]) & (scaled_sobel <= sx_thresh[1])] = 1
+    
+    # Threshold color channel
+    s_binary = np.zeros_like(s_channel)
+    s_binary[(s_channel >= s_thresh[0]) & (s_channel <= s_thresh[1])] = 1
+    # Stack each channel
+    # Note color_binary[:, :, 0] is all 0s, effectively an all black image. It might
+    # be beneficial to replace this channel with something else.
+    color_binary = np.dstack(( sxbinary, np.zeros_like(sxbinary), s_binary))
+    return color_binary
+
+
+def pipeline(img,warp_Matrix):
+	img_undistort = undistort(img)
+	colorGrad = colorGradient(img_undistort)
+	unwarped = warp(colorGrad,warp_Matrix)
+	return unwarped
+
+
+
 # Read in the saved camera matrix and distortion coefficients
 # These are the arrays you calculated using cv2.calibrateCamera()
 dist_pickle = pickle.load( open( "../camera_cal/camera_calibration_pickle.p", "rb" ) )
 mtx = dist_pickle["mtx"]
 dist = dist_pickle["dist"]
-
+M = dist_pickle["M"]
 img = cv2.imread('../test_images/test5.jpg')
 
-def undistort(img):
-	return cv2.undistort(img, mtx, dist, None, mtx)
+# now calculate M somehow.....
 
-
-def pipeline(img):
-	img_undistort = undistort(img)
-	return img_undistort
-
-img_pipelined = pipeline(img)
+img_pipelined = pipeline(img,M)
 
 
 f, (ax1, ax2) = plt.subplots(1, 2, figsize=(20,10))
